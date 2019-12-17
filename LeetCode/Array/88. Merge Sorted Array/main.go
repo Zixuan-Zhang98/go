2e45069898512dec2c84e921bbced70f3494231e
Zixuan Zhang <zhan7230@umn.edu>
2019-12-17 16:06:27 -0600

    Add the description for the LeetCode Array question: 88. Merge Sorted Array

--- LeetCode/Array/88. Merge Sorted Array/main.go
+++ LeetCode/Array/88. Merge Sorted Array/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+//Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+//
+//Note:
+//
+//The number of elements initialized in nums1 and nums2 are m and n respectively.
+//You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+//Example:
+//
+//Input:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//Output: [1,2,2,3,5,6]
+
 func main() {
 	nums1 := make([]int, 6)
 	nums1[0] = 1
