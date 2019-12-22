b064958773fe5d067ccefeb4b9129f8e4fcead87
Zixuan Zhang <zhan7230@umn.edu>
2019-12-22 16:07:02 -0600

    Add http files

---
+++ Web/04_nethttp/01_understanding-net-http-package/03_Request/03_URL/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"text/template"
+)
+
+type hotdog int
+
+func (d hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	data := struct {
+		Method      string
+		URL         *url.URL
+		Submissions url.Values
+	}{
+		r.Method,
+		r.URL,
+		r.Form,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
