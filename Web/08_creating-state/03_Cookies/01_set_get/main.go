41c5b98c65fe6c82e33a0fad9e30504ddac6edac
Zixuan Zhang <zhan7230@umn.edu>
2019-12-24 20:51:13 -0600

    Add state files

---
+++ Web/08_creating-state/03_Cookies/01_set_get/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", set)
+	http.HandleFunc("/read", read)
+	http.ListenAndServe(":8080", nil)
+}
+
+func set(w http.ResponseWriter, r *http.Request) {
+	http.SetCookie(w, &http.Cookie{
+		Name:  "my-cookie",
+		Value: "some value",
+		Path:  "/",
+	})
+	fmt.Fprintln(w, "COOKIE WRITTEN - CHECK YOUR BROWSER")
+	fmt.Fprintln(w, "in chrome go to: dev tools / application / cookies")
+}
+
+func read(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("my-cookie")
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintln(w, "YOUR COOKIE:", c)
+}
