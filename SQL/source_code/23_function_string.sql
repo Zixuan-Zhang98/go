140b22a2cedba38b2556f6a824c014fa5fe39c0f
Zixuan Zhang <zhan7230@umn.edu>
2019-12-30 18:11:28 -0600

    Add sql files

--- SQL/source_code/23_function_string.sql
+++ SQL/source_code/23_function_string.sql
@@ -12,8 +12,8 @@ SELECT CONCAT('a','b','c');
 SELECT CONCAT('a','b','c',null);
 
 -- CONCAT_WS(x,s1,s2,s2....)：以指定分隔符拼接字符串
-SELECT CONCAT_WS('-','a','b','c');
--- 如果null在拼接的内容中，则转化成空字符串
+SELECT CONCAT_WS('-','a','b','c'); -- With Separator
+-- 如果null在拼接的内容中，则转化成空字符串; NULL被忽略
 SELECT CONCAT_WS('-','a','b','c',null);
 -- 如果分隔符为null，拼接的结果为null
 SELECT CONCAT_WS(null,'a','b','c');
@@ -42,8 +42,8 @@ SELECT REPEAT('hello',3);
 SELECT REPLACE('hello king','king','queen');
 
 -- 截取字符串SUBSTRING
-SELECT SUBSTRING('abcdef',1,3);
+SELECT SUBSTRING('abcdef',1,3); -- 从第1位开始截取3位
 
 -- 比较字符串
 
-SELECT STRCMP('a','b');
+SELECT STRCMP('a','b'); --返回-1，0，1
